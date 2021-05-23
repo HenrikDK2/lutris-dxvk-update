9c4c69213335de1ac533ad59349f4a2ffe946171
Henrik <Henrik265@outlook.dk>
2021-05-23 15:46:03 +0200

    Now checks if latest version is installed

--- update.sh
+++ update.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-rm -rf dxvk
-git clone https://github.com/doitsujin/dxvk dxvk
-cd dxvk
-rm -rf ~/.local/share/lutris/runtime/dxvk/latest/
-./package-release.sh master ~/.local/share/lutris/runtime/dxvk/latest/ --no-package
-rm -rf dxvk
-cd ~/.local/share/lutris/runtime/dxvk/latest/dxvk-master
-cp -r x32 ..
-cp -r x64 ..
-cd ..
-rm -rf dxvk-master
+update(){
+    rm -rf dxvk
+    git clone https://github.com/doitsujin/dxvk dxvk
+    cd dxvk
+    rm -rf ~/.local/share/lutris/runtime/dxvk/latest/
+    ./package-release.sh master ~/.local/share/lutris/runtime/dxvk/latest/ --no-package
+    rm -rf dxvk
+    cd ~/.local/share/lutris/runtime/dxvk/latest/dxvk-master
+    cp -r x32 ..
+    cp -r x64 ..
+    cd ..
+    rm -rf dxvk-master
+    echo "$(git log -1 --format=%ct)000" > last_download
+}
+
+if test -f "last_download";
+    then
+        if [ "$(git log -1 --format=%ct)000" -gt "$(<last_download)" ]; 
+            then
+                update
+            else
+                echo "Lutris DXVK Update: Latest version is already installed"
+        fi
+    else
+        update
+    fi
+
+
+
